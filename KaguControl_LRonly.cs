706efc45d35f2b4259c3de6505fff829448e55f8
TenshouSu <tenshou.su@f.ait.kyushu-u.ac.jp>
2017-12-18 14:07:47 +0900

    Update KaguControl_LRonly.cs

--- KaguControl_LRonly.cs
+++ KaguControl_LRonly.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class sofaControl : MonoBehaviour {
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;
-			if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
-				float x = hit.point.x;
-				float z = hit.point.z;
-			}
+public class ObjectRotation : MonoBehaviour {
+
+	private float xSpeed = 200.0f;
+
+	private float x = 0.0f;
+
+	private Quaternion rotation = Quaternion.Euler(new Vector3(30f,0f,0f));
+
+	void Update ()
+	{
+
+		if(Input.GetMouseButton(0))
+		{
+			x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+			var rotation = Quaternion.Euler(0, x, 0);
+			transform.rotation = rotation;
 		}
+
 	}
+	
 }
