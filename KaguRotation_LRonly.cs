c42525816a7d3c3d9332aaa82ba8adc1b8461a15
TenshouSu <tenshou.su@f.ait.kyushu-u.ac.jp>
2017-12-19 14:32:33 +0900

    Update KaguRotation_LRonly.cs

--- KaguRotation_LRonly.cs
+++ KaguRotation_LRonly.cs
@@ -8,7 +8,7 @@ public class KaguRotation_LRonly : MonoBehaviour {
 
 	private float xSpeed = 200.0f;
 
-	private float x = 0.0f;
+	private float xKaiten = 0.0f;
 
 	private Quaternion rotation = Quaternion.Euler(new Vector3(0f,0f,0f));
 
@@ -17,8 +17,8 @@ public class KaguRotation_LRonly : MonoBehaviour {
 
 		if(Input.GetMouseButton(0))
 		{
-			x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
-			var rotation = Quaternion.Euler(0, x, 0);
+			xKaiten += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+			var rotation = Quaternion.Euler(0, xKaiten, 0);
 			transform.rotation = rotation;
 		}
 
